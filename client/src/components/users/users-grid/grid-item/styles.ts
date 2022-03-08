c7e56cef20d8918c2ab0597a2e384f943f8ee99e
ElderSam <eldersamuel98@gmail.com>
2022-03-08 16:18:40 -0300

    style: Home - grid items hover effect

--- client/src/components/users/users-grid/grid-item/styles.ts
+++ client/src/components/users/users-grid/grid-item/styles.ts
@@ -4,6 +4,12 @@ export const GridUserItem = styled.div`
 	background-color: #e6dbf1;
     border-radius: 15px;
     padding: 15px;
+    transition: 0.5s;
+
+    &:hover {
+        background-color: #debdff;
+        border-radius: 7px;
+    }
 `;
 
 export const CardBody = styled.div`
