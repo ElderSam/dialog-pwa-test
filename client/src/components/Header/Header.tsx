46dde6666b661c5381191848969cb41f181beeb6
ElderSam <eldersamuel98@gmail.com>
2022-03-08 20:50:14 -0300

    feat: Header - updading input value

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppHeader } from './styles'
 import { ReactComponent as SearchLogo } from '../../components/icons/search.svg';
 
 function Header() {
+	const [search, setSearch] = useState();
+
+	const handleChange = (e: any): void => {
+		if(e?.target?.value) {
+			setSearch(e?.target?.value)
+		}
+	};
+
 	return (
 		<AppHeader>
 			<p>MySocial</p>
 
 			<div>
-				<input type="text" placeholder="search" />
-				<button>
+				<input type="text" onChange={handleChange} placeholder="search" />
+				<button onClick={() => console.log('search', search)}>
 					<SearchLogo height="25" />
 				</button>
 			</div>
