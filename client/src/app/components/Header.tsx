29a2fdedb868eb01f88c8b9ad336eadca0d7979c
ElderSam <eldersamuel98@gmail.com>
2022-03-08 14:23:45 -0300

    feat: add Header component

---
+++ client/src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppHeader } from './styles'
+
+function Header() {
+	return (
+		<AppHeader>
+			<p>MySocial</p>
+		</AppHeader>
+	);
+}
+
+export default Header;
